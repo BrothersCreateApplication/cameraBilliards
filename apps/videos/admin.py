bd56559e77b05a0b44f5ef9039be8768a899246c
pktdanh <pktdanh@gmail.com>
2024-03-08 09:25:39 +0700

    Update models

--- apps/videos/admin.py
+++ apps/videos/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Video, Frame
+from django.utils.html import format_html
 # Register your models here.
 
 @admin.register(Video)
@@ -15,9 +16,10 @@ class Video(admin.ModelAdmin):
   )
 
 @admin.register(Frame)
-class Video(admin.ModelAdmin):
+class Frame(admin.ModelAdmin):
   list_display = (
     "id",
+    "preview_image",
     "start_time",
     "end_time",
     "created_at",
@@ -25,3 +27,9 @@ class Video(admin.ModelAdmin):
   search_fields = (
     "id",
   )
+  readonly_fields = ('preview_image',)
+
+  def preview_image(self, obj):
+      return format_html('<img src="data:image/jpeg;base64,{}" style="max-width:200px;max-height:200px;">',
+                           obj.image_data)
+  preview_image.short_description = 'Image Preview'
