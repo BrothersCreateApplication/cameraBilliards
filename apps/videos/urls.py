4e45f931ae497f23fb25e34c216aefea6ab2b051
pktdanh <pktdanh@gmail.com>
2024-03-19 09:41:53 +0700

    Update models

--- apps/videos/urls.py
+++ apps/videos/urls.py
@@ -2,12 +2,13 @@ from rest_framework.routers import SimpleRouter
 from django.urls import path
 # from django.urls import include, re_path
 from .views import (
-    TestView, segment_list_view, upload_video, show_video
+    TestView, 
+    # segment_list_view, upload_video, show_video
 )
 
 urlpatterns = [
     path("test", TestView.as_view(), name="test"),
-    path('<str:video_id>/segment/', segment_list_view, name='segment_list'),
-    path('upload/', upload_video, name='upload_video'),
-    path('<str:video_id>/', show_video, name='show_video'),
+    # path('<str:video_id>/segment/', segment_list_view, name='segment_list'),
+    # path('upload/', upload_video, name='upload_video'),
+    # path('<str:video_id>/', show_video, name='show_video'),
 ]
--- apps/videos/views.py
+++ apps/videos/views.py
@@ -5,10 +5,9 @@ from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi as yasg_openapi
 from django.http import StreamingHttpResponse
-from .models import Frame, Video
 from apps.users.models import User
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import VideoForm
+# from .forms import VideoForm
 from django.urls import reverse
 
 class TestView(APIView):
@@ -16,22 +15,22 @@ class TestView(APIView):
     users = User.objects.all()
     return Response("Hello world!!!")
 
-def segment_list_view(request, video_id):
-    video = Video.objects.get(pk=video_id)
-    segments = Frame.objects.filter(video=video)
-    return render(request, 'videos/segment_list.html', {'video': video, 'segments': segments})
+# def segment_list_view(request, video_id):
+#     video = Video.objects.get(pk=video_id)
+#     segments = Frame.objects.filter(video=video)
+#     return render(request, 'videos/segment_list.html', {'video': video, 'segments': segments})
 
 
-def upload_video(request):
-    if request.method == 'POST':
-        form = VideoForm(request.POST, request.FILES)
-        if form.is_valid():
-            video = form.save()
-            return redirect(reverse('videos:show_video', args=[video.id])) 
-    else:
-        form = VideoForm()
-    return render(request, 'videos/upload_video.html', {'form': form})
+# def upload_video(request):
+#     if request.method == 'POST':
+#         form = VideoForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             video = form.save()
+#             return redirect(reverse('videos:show_video', args=[video.id])) 
+#     else:
+#         form = VideoForm()
+#     return render(request, 'videos/upload_video.html', {'form': form})
 
-def show_video(request, video_id):
-    video = get_object_or_404(Video, pk=video_id)
-    return render(request, 'videos/show_video.html', {'video': video})
+# def show_video(request, video_id):
+#     video = get_object_or_404(Video, pk=video_id)
+#     return render(request, 'videos/show_video.html', {'video': video})
